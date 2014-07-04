1581ed6d03bee5b9e76708b6df53256fc1fddc05
overherz <overherz@gmail.com>
2014-07-04 21:27:36 +0400

    1

---
+++ data/layouts_cache/20/44/60f784b1fa728ecc2aaa55032a86b1b89a3acddc6ca192d9b74da1fd0d41.php
@@ -0,0 +1,110 @@
+<?php
+
+/* applications/projects/layouts/calendar/months.html */
+class __TwigTemplate_204460f784b1fa728ecc2aaa55032a86b1b89a3acddc6ca192d9b74da1fd0d41 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo \layout::func_from_text("<div>
+    <div style=\"float: right;\">
+        <a href=\"\">Актуальная дата</a></div>
+</div>
+
+<section class=\"month\" style=\"clear: both;\">
+    <h2 style=\"text-align: center;\">
+        <a class=\"arrow\" href=\"\" change_year=\"dec\">&larr;</a>
+        <span id=\"year\">");
+        // line 9
+        echo \layout::func_from_text(twig_escape_filter($this->env, (isset($context["year"]) ? $context["year"] : null), "html", null, true));
+        echo \layout::func_from_text("</span>
+        <a class=\"arrow\" href=\"\" change_year=\"inc\">&rarr;</a>
+    </h2>
+    <input type=\"hidden\" name=\"year\" value=\"");
+        // line 12
+        echo \layout::func_from_text(twig_escape_filter($this->env, (isset($context["year"]) ? $context["year"] : null), "html", null, true));
+        echo \layout::func_from_text("\">
+    <table class=\"table table-bordered months\">
+        <tr>
+            <td set_month=\"1\">");
+        // line 15
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_1"), "html", null, true));
+        echo \layout::func_from_text("</td>
+            <td set_month=\"2\">");
+        // line 16
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_2"), "html", null, true));
+        echo \layout::func_from_text("</td>
+            <td set_month=\"3\">");
+        // line 17
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_3"), "html", null, true));
+        echo \layout::func_from_text("</td>
+            <td set_month=\"4\">");
+        // line 18
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_4"), "html", null, true));
+        echo \layout::func_from_text("</td>
+        </tr>
+        <tr>
+            <td set_month=\"5\">");
+        // line 21
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_5"), "html", null, true));
+        echo \layout::func_from_text("</td>
+            <td set_month=\"6\">");
+        // line 22
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_6"), "html", null, true));
+        echo \layout::func_from_text("</td>
+            <td set_month=\"7\">");
+        // line 23
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_7"), "html", null, true));
+        echo \layout::func_from_text("</td>
+            <td set_month=\"8\">");
+        // line 24
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_8"), "html", null, true));
+        echo \layout::func_from_text("</td>
+        </tr>
+        <tr>
+            <td set_month=\"9\">");
+        // line 27
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_9"), "html", null, true));
+        echo \layout::func_from_text("</td>
+            <td set_month=\"10\">");
+        // line 28
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_10"), "html", null, true));
+        echo \layout::func_from_text("</td>
+            <td set_month=\"11\">");
+        // line 29
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_11"), "html", null, true));
+        echo \layout::func_from_text("</td>
+            <td set_month=\"12\">");
+        // line 30
+        echo \layout::func_from_text(twig_escape_filter($this->env, lang("month_12"), "html", null, true));
+        echo \layout::func_from_text("</td>
+        </tr>
+    </table>
+</section>");
+    }
+
+    public function getTemplateName()
+    {
+        return "applications/projects/layouts/calendar/months.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  89 => 30,  85 => 29,  81 => 28,  77 => 27,  71 => 24,  67 => 23,  63 => 22,  59 => 21,  53 => 18,  49 => 17,  45 => 16,  41 => 15,  35 => 12,  29 => 9,  19 => 1,);
+    }
+}
---
+++ data/layouts_cache/d9/ec/300ef0ca7205b43e12a721e2dfcb3299d8ebf7a887d7cb1a3af7d0f27ff3.php
@@ -0,0 +1,95 @@
+<?php
+
+/* applications/projects/layouts/user_tasks.html */
+class __TwigTemplate_d9ec300ef0ca7205b43e12a721e2dfcb3299d8ebf7a887d7cb1a3af7d0f27ff3 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo \layout::func_from_text("    <script src=\"/source/js/jquery.ui.datepicker-ru.min.js\"></script>
+    <script type=\"text/javascript\" src=\"/source/js/search.js\"></script>
+    <script type=\"text/javascript\" src=\"");
+        // line 3
+        echo \layout::func_from_text(twig_escape_filter($this->env, $this->getAttribute((isset($context["app"]) ? $context["app"] : null), "path", array(0 => "projects", 1 => "user_tasks.js"), "method"), "html", null, true));
+        echo \layout::func_from_text("\"></script>
+    <ul class=\"breadcrumbs-one second\">
+        <li><a class=\"current\">Задачи</a></li>
+    </ul>
+    ");
+        // line 7
+        $context["inputs"] = $this->env->loadTemplate("/source/search_macro.html");
+        // line 8
+        echo \layout::func_from_text("    <form path=\"/projects/user_tasks/\" id=\"search_form\" method=\"post\" class=\"form-horizontal user_tasks\" style=\"margin-bottom:0px;\">
+        <input type=\"hidden\" name=\"page\" value=\"\">
+        <input type=\"hidden\" name=\"act\" value=\"get_data\">
+        <input type=\"hidden\" name=\"id_user\" value=\"");
+        // line 11
+        echo \layout::func_from_text(twig_escape_filter($this->env, (isset($context["id_user"]) ? $context["id_user"] : null), "html", null, true));
+        echo \layout::func_from_text("\">
+        <table class=\"table table-condensed table-border\" style=\"width: auto;\">
+            <tr>
+                <th>Дата</th>
+                <th>Статус</th>
+                <th>Приоритет</th>
+                <th>Другое</th>
+            </tr>
+            <tr>
+                <td>От: <input type=\"text\" name=\"start\" value=\"");
+        // line 20
+        echo \layout::func_from_text(twig_escape_filter($this->env, (isset($context["start"]) ? $context["start"] : null), "html", null, true));
+        echo \layout::func_from_text("\" class=\"input-small\" readonly style=\"margin-bottom: 10px;\"><br>
+                    До: <input type=\"text\" name=\"end\" value=\"");
+        // line 21
+        echo \layout::func_from_text(twig_escape_filter($this->env, (isset($context["end"]) ? $context["end"] : null), "html", null, true));
+        echo \layout::func_from_text("\" class=\"input-small\" readonly></td>
+                <td>");
+        // line 22
+        echo \layout::func_from_text($context["inputs"]->getinput($this->getAttribute((isset($context["form"]) ? $context["form"] : null), "status"), "status"));
+        echo \layout::func_from_text("</td>
+                <td>");
+        // line 23
+        echo \layout::func_from_text($context["inputs"]->getinput($this->getAttribute((isset($context["form"]) ? $context["form"] : null), "priority"), "priority"));
+        echo \layout::func_from_text("</td>
+                <td>
+                    <input type=\"text\" name=\"search\" id=\"search_label\" class=\"input-large\" placeholder=\"Поиск\"><br/><br/>
+                    ");
+        // line 26
+        echo \layout::func_from_text($context["inputs"]->getinput($this->getAttribute((isset($context["form"]) ? $context["form"] : null), "percent"), "percent", true));
+        echo \layout::func_from_text("
+                </td>
+            </tr>
+        </table>
+    </form>
+    <div class=\"clearfix\"></div>
+    <div id=\"search_result\">");
+        // line 32
+        $template = $this->env->resolveTemplate($this->getAttribute((isset($context["app"]) ? $context["app"] : null), "path", array(0 => "projects", 1 => "calendar/task_today.html"), "method"));
+        $template->display($context);
+        echo \layout::func_from_text("</div>");
+    }
+
+    public function getTemplateName()
+    {
+        return "applications/projects/layouts/user_tasks.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  76 => 32,  67 => 26,  61 => 23,  57 => 22,  53 => 21,  49 => 20,  37 => 11,  32 => 8,  30 => 7,  23 => 3,  19 => 1,);
+    }
+}
