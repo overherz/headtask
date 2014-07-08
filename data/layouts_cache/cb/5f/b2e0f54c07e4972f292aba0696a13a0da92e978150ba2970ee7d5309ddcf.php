5613df3039dd9f12195ff87e1c040a888d0dbf84
overherz <overherz@gmail.com>
2014-07-08 17:58:50 +0400

    1

--- data/layouts_cache/cb/5f/b2e0f54c07e4972f292aba0696a13a0da92e978150ba2970ee7d5309ddcf.php
+++ data/layouts_cache/cb/5f/b2e0f54c07e4972f292aba0696a13a0da92e978150ba2970ee7d5309ddcf.php
@@ -12,8 +12,8 @@ class __TwigTemplate_cb5fb2e0f54c07e4972f292aba0696a13a0da92e978150ba2970ee7d530
         $this->blocks = array(
             'title' => array($this, 'block_title'),
             'css' => array($this, 'block_css'),
-            'js' => array($this, 'block_js'),
             'body' => array($this, 'block_body'),
+            'js' => array($this, 'block_js'),
         );
     }
 
@@ -42,79 +42,80 @@ class __TwigTemplate_cb5fb2e0f54c07e4972f292aba0696a13a0da92e978150ba2970ee7d530
         // line 16
         $this->displayBlock('css', $context, $blocks);
         // line 18
-        echo \layout::func_from_text("    <script src=\"http://yandex.st/jquery/2.1.1/jquery.min.js\"></script>
-    <script src=\"http://yandex.st/jquery-ui/1.10.4/jquery-ui.min.js\"></script>
-    <script src=\"/source/js/jquery.jgrowl.min.js\"></script>
-    <script src=\"/source/js/functions.js\"></script>
-    <script src=\"/source/js/message.js\"></script>
-    <script src=\"http://yandex.st/bootstrap/3.1.1/js/bootstrap.min.js\"></script>
-    <script src=\"/source/js/styler/jquery.formstyler.min.js\"></script>
-    <script>
-        \$(document).ready(function(\$) {
-            \$('#wrap input, #wrap select').styler();
-        });
-    </script>
-    <script src=\"/source/js/sound_manager/soundmanager2-nodebug-jsmin.js\"></script>
-    <script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js\"></script>
-    <script type=\"text/javascript\">
-        \$(document).ready(function(\$) {
-            \$.getScript(\"/source/js/socket.io/socket.io.js\",function(){
-                \$.getScript(\"");
-        // line 35
-        echo \layout::func_from_text(twig_escape_filter($this->env, $this->getAttribute((isset($context["app"]) ? $context["app"] : null), "path", array(0 => "users", 1 => "client.js"), "method"), "html", null, true));
-        echo \layout::func_from_text("\",function(){
-                    soundManager.setup({
-                        useFlashBlock : false,
-                        url : '/source/js/sound_manager/',
-                        debugMode : false,
-                        consoleOnly : false,
-                        onready : function() {
-                            soundManager.createSound({
-                                id:'new_message',
-                                url:'/source/js/sound_manager/sound.mp3'
-                            });
-                            soundManager.createSound({
-                                id:'call',
-                                url:'/source/js/sound_manager/incoming_call.mp3'
-                            });
-                        }
-                    });
-                });
-            });
-        });
-
-        window.ms = {
-            address: \"");
-        // line 57
-        echo \layout::func_from_text(twig_escape_filter($this->env, $this->getAttribute((isset($context["globals"]) ? $context["globals"] : null), "message_server"), "html", null, true));
-        echo \layout::func_from_text("\",
-            uniq_key: \"");
-        // line 58
-        echo \layout::func_from_text(twig_escape_filter($this->env, $this->getAttribute($this->getAttribute((isset($context["globals"]) ? $context["globals"] : null), "user"), "uniq_key"), "html", null, true));
-        echo \layout::func_from_text("\"
-        }
-    </script>
-    ");
-        // line 61
-        $this->displayBlock('js', $context, $blocks);
-        // line 63
         echo \layout::func_from_text("</head>
 <body>
 <div id=\"wrap\">
     ");
-        // line 66
+        // line 21
         $this->env->loadTemplate("/source/menu.html")->display($context);
-        // line 67
+        // line 22
         echo \layout::func_from_text("    <div class=\"from_top\"></div>
     ");
-        // line 68
+        // line 23
         $this->displayBlock('body', $context, $blocks);
-        // line 69
+        // line 24
         echo \layout::func_from_text("</div>
 ");
-        // line 70
+        // line 25
         $this->env->loadTemplate("/source/footer.html")->display($context);
-        // line 71
+        // line 26
+        echo \layout::func_from_text("
+<script src=\"http://yandex.st/jquery/2.1.1/jquery.min.js\"></script>
+<script src=\"http://yandex.st/jquery-ui/1.10.4/jquery-ui.min.js\"></script>
+<script src=\"/source/js/jquery.jgrowl.min.js\"></script>
+<script src=\"/source/js/functions.js\"></script>
+<script src=\"/source/js/message.js\"></script>
+<script src=\"http://yandex.st/bootstrap/3.1.1/js/bootstrap.min.js\"></script>
+<script src=\"/source/js/styler/jquery.formstyler.min.js\"></script>
+<script>
+    \$(document).ready(function(\$) {
+        \$('#wrap input, #wrap select').styler();
+    });
+</script>
+<script src=\"/source/js/sound_manager/soundmanager2-nodebug-jsmin.js\"></script>
+<script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js\"></script>
+<script type=\"text/javascript\">
+    \$(document).ready(function(\$) {
+        \$.getScript(\"/source/js/socket.io/socket.io.js\",function(){
+            \$.getScript(\"");
+        // line 44
+        echo \layout::func_from_text(twig_escape_filter($this->env, $this->getAttribute((isset($context["app"]) ? $context["app"] : null), "path", array(0 => "users", 1 => "client.js"), "method"), "html", null, true));
+        echo \layout::func_from_text("\",function(){
+                soundManager.setup({
+                    useFlashBlock : false,
+                    url : '/source/js/sound_manager/',
+                    debugMode : false,
+                    consoleOnly : false,
+                    onready : function() {
+                        soundManager.createSound({
+                            id:'new_message',
+                            url:'/source/js/sound_manager/sound.mp3'
+                        });
+                        soundManager.createSound({
+                            id:'call',
+                            url:'/source/js/sound_manager/incoming_call.mp3'
+                        });
+                    }
+                });
+            });
+        });
+    });
+
+    window.ms = {
+        address: \"");
+        // line 66
+        echo \layout::func_from_text(twig_escape_filter($this->env, $this->getAttribute((isset($context["globals"]) ? $context["globals"] : null), "message_server"), "html", null, true));
+        echo \layout::func_from_text("\",
+        uniq_key: \"");
+        // line 67
+        echo \layout::func_from_text(twig_escape_filter($this->env, $this->getAttribute($this->getAttribute((isset($context["globals"]) ? $context["globals"] : null), "user"), "uniq_key"), "html", null, true));
+        echo \layout::func_from_text("\"
+    }
+</script>
+");
+        // line 70
+        $this->displayBlock('js', $context, $blocks);
+        // line 72
         echo \layout::func_from_text("</body>
 </html>");
     }
@@ -131,15 +132,13 @@ class __TwigTemplate_cb5fb2e0f54c07e4972f292aba0696a13a0da92e978150ba2970ee7d530
         echo \layout::func_from_text("    ");
     }
 
-    // line 61
-    public function block_js($context, array $blocks = array())
+    // line 23
+    public function block_body($context, array $blocks = array())
     {
-        // line 62
-        echo \layout::func_from_text("    ");
     }
 
-    // line 68
-    public function block_body($context, array $blocks = array())
+    // line 70
+    public function block_js($context, array $blocks = array())
     {
     }
 
@@ -155,6 +154,6 @@ class __TwigTemplate_cb5fb2e0f54c07e4972f292aba0696a13a0da92e978150ba2970ee7d530
 
     public function getDebugInfo()
     {
-        return array (  142 => 68,  138 => 62,  135 => 61,  131 => 17,  128 => 16,  123 => 4,  118 => 71,  116 => 70,  113 => 69,  111 => 68,  108 => 67,  106 => 66,  101 => 63,  99 => 61,  93 => 58,  89 => 57,  45 => 18,  28 => 4,  23 => 1,  48 => 7,  40 => 8,  38 => 7,  32 => 3,  29 => 2,  80 => 26,  76 => 24,  69 => 23,  64 => 35,  61 => 19,  56 => 17,  46 => 10,  43 => 16,  34 => 3,  31 => 2,);
+        return array (  141 => 70,  136 => 23,  132 => 17,  129 => 16,  124 => 4,  119 => 72,  117 => 70,  111 => 67,  107 => 66,  82 => 44,  62 => 26,  60 => 25,  57 => 24,  55 => 23,  52 => 22,  50 => 21,  45 => 18,  43 => 16,  28 => 4,  23 => 1,);
     }
 }
