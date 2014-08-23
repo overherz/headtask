f304b8735a2663f1b90898d56298c5abf4a1f30f
overherz <overherz@gmail.com>
2014-08-23 04:54:24 +0400

    new design

--- data/layouts_cache/7c/06/a6203429c3ae65842d826294fd4688b0416043e08d49272cf950116449a1.php
+++ data/layouts_cache/7c/06/a6203429c3ae65842d826294fd4688b0416043e08d49272cf950116449a1.php
@@ -16,23 +16,26 @@ class __TwigTemplate_7c06a6203429c3ae65842d826294fd4688b0416043e08d49272cf950116
     protected function doDisplay(array $context, array $blocks = array())
     {
         // line 1
+        echo \layout::func_from_text("<ul style=\"list-style: none;padding-left: 0;\">
+");
+        // line 2
         $context['_parent'] = (array) $context;
         $context['_seq'] = twig_ensure_traversable($this->getAttribute((isset($context["projects"]) ? $context["projects"] : null), "projects"));
         foreach ($context['_seq'] as $context["_key"] => $context["p"]) {
-            // line 2
-            echo \layout::func_from_text("    <li style=\"text-indent: 30px;\" class=\"sidebar_project ");
+            // line 3
+            echo \layout::func_from_text("    <li style=\"text-indent: 0;\" class=\"sidebar_project ");
             if ((($this->getAttribute((isset($context["project"]) ? $context["project"] : null), "id") == $this->getAttribute((isset($context["p"]) ? $context["p"] : null), "id")) || ($this->getAttribute((isset($context["projects"]) ? $context["projects"] : null), "current_project") == $this->getAttribute((isset($context["p"]) ? $context["p"] : null), "id")))) {
                 echo \layout::func_from_text("active");
             }
             echo \layout::func_from_text("\">
     <a href=\"/projects/tasks/");
-            // line 3
+            // line 4
             echo \layout::func_from_text(twig_escape_filter($this->env, $this->getAttribute((isset($context["p"]) ? $context["p"] : null), "id"), "html", null, true));
             echo \layout::func_from_text("/\" data-id=\"");
             echo \layout::func_from_text(twig_escape_filter($this->env, $this->getAttribute((isset($context["p"]) ? $context["p"] : null), "id"), "html", null, true));
-            echo \layout::func_from_text("\">
+            echo \layout::func_from_text("\" style=\"padding: 5px 0 5px 30px;line-height: 15px;\">
         ");
-            // line 4
+            // line 5
             if ($this->getAttribute((isset($context["p"]) ? $context["p"] : null), "owner")) {
                 echo \layout::func_from_text("<i class=\"fa fa-user\" style=\"text-indent: 0;\"></i> ");
             }
@@ -45,9 +48,8 @@ class __TwigTemplate_7c06a6203429c3ae65842d826294fd4688b0416043e08d49272cf950116
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['p'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 8
-        echo \layout::func_from_text("
-
+        // line 9
+        echo \layout::func_from_text("</ul>
 ");
         // line 10
         $template = $this->env->resolveTemplate($this->getAttribute((isset($context["app"]) ? $context["app"] : null), "path", array(0 => "projects", 1 => "jpaginator_project_panel.html"), "method"));
@@ -66,6 +68,6 @@ class __TwigTemplate_7c06a6203429c3ae65842d826294fd4688b0416043e08d49272cf950116
 
     public function getDebugInfo()
     {
-        return array (  53 => 10,  49 => 8,  36 => 4,  30 => 3,  23 => 2,  19 => 1,);
+        return array (  55 => 10,  52 => 9,  39 => 5,  33 => 4,  26 => 3,  22 => 2,  19 => 1,);
     }
 }
