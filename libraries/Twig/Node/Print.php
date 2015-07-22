25a999251448cfb5a0138e058c798c92bc2f9ef4
overherz <overherz@gmail.com>
2015-07-23 03:18:32 +0400

    twig bump version

--- libraries/Twig/Node/Print.php
+++ libraries/Twig/Node/Print.php
@@ -25,9 +25,8 @@ class Twig_Node_Print extends Twig_Node implements Twig_NodeOutputInterface
     /**
      * Compiles the node to PHP.
      *
-     * @param Twig_Compiler A Twig_Compiler instance
+     * @param Twig_Compiler $compiler A Twig_Compiler instance
      */
-
     public function compile(Twig_Compiler $compiler)
     {
         $compiler
